725e6db8cdcdf944b221166e6184f29eceb3a97d
RSLastra <raul.slastra@gmail.com>
2023-11-01 21:53:03 -0600

    añadiendo links a redes

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,6 +1,7 @@
-import imagen from '/src/img/foto_raul.jpeg'
-import ButtonOne from './Button'
-import { AiOutlineDownload, AiFillLinkedin, AiFillGithub} from 'react-icons/ai'
+import imagen from '/src/img/foto_raul.jpeg';
+import ButtonOne from './Button';
+import { AiOutlineDownload, AiFillLinkedin, AiFillGithub } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
 
 const Intro = () => {
     return (
@@ -11,9 +12,13 @@ const Intro = () => {
                 {/* <h1 className="text-center sm:text-left text-3xl sm:text-5xl font-semibold "></h1> */}
                 <p className='sm:text-lg mt-4 md:mt-8 text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni error perspiciatis quae quod? Quaerat veritatis voluptatem, sed eum eligendi quas, quae maiores suscipit deleniti, iste sapiente beatae voluptas perferendis non?</p>
                 <div className='flex gap-5 justify-center sm:justify-normal items-center mt-6'>
-                    <ButtonOne label="Download CV" icon={<AiOutlineDownload size={20}/>} />
-                    <AiFillLinkedin className='text-3xl sm:text-5xl cursor-pointer hover:scale-[1.08]'/>
-                    <AiFillGithub  className='text-3xl sm:text-5xl cursor-pointer hover:scale-[1.08]'/>
+                    <ButtonOne label="Download CV" icon={<AiOutlineDownload size={20} />} />
+                    <Link to='https://www.linkedin.com/in/ra%C3%BAl-su%C3%A1rez-906754270/' target='_blank'>
+                        <AiFillLinkedin className='text-3xl sm:text-5xl cursor-pointer hover:scale-[1.08]' />
+                    </Link>
+                    <Link to='https://github.com/RSuarezLastra' target='_blank'>
+                        <AiFillGithub className='text-3xl sm:text-5xl cursor-pointer hover:scale-[1.08]' />
+                    </Link>
                 </div>
             </div>
             <div className="flex justify-center w-full mt-10 md:mt-0">
