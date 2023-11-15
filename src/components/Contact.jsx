5f68cc8016df169cfb40d4d05477aab341226e4a
RSLastra <raul.slastra@gmail.com>
2023-11-15 17:50:54 -0600

    empezando form y agregando material ui

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,13 +1,53 @@
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import ButtonOne from './Button'
 
+const Contact = () => {
+    return (
+        <section className='h-screen w-3/4 mx-auto '>
 
-const Contact = () =>{
-    return(
-        <div>
-            <h2 className="text-center font-semibold text-3xl mb-8 sm:mb-20 dark:text-slate-200">Contact me</h2>
-            <form action="">
+            <div className=' bg-seance-950 px-6 grid grid-cols-2 rounded-xl'>
+                <div className='py-10 px-4'>
+                    <h2 className='text-2xl text-white mb-10'>Contact Information</h2>
+                    <p className='text-2xl text-white'>Diego se la come</p>
+                </div>
+                <div className='py-10 px-4'>
+                    <Box
+                        className='bg-slate-100 rounded-xl p-5'
+                        component="form">
+                        <h2 className="text-center font-semibold text-3xl mb-8 ">CONTACT <span className='text-seance-700'>ME</span></h2>
+                        <div >
+                            <TextField
+                                id='standard-basic'
+                                label='Your Name'
+                                type='text'
+                                variant='standard'
+                                fullWidth />
+                        </div>
+                        <div className='mt-10'>
+                            <TextField
+                                id='standard-basic'
+                                label='Your Email'
+                                type='email'
+                                variant='standard'
+                                fullWidth />
+                        </div>
+                        <div className='mt-10'>
+                            <TextField
+                                id='standard-basic'
+                                label='Your Message'
+                                type='email'
+                                variant='standard'
+                                fullWidth />
+                        </div>
+                        <div className='mt-20 grid place-items-end'>
+                            <ButtonOne label='Send' />
+                        </div>
+                    </Box>
 
-            </form>
-        </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -5,8 +5,8 @@ const Intro = () => {
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 gap-0 md:gap-10 sm:min-h-screen p-4 md:p-10 lg:p-16 w-10/12 mx-auto">
             <div className="flex flex-col justify-center">
-                <h1 className="text-center sm:text-left text-3xl sm:text-5xl font-semibold mb-1 lg:mb-4 dark:text-slate-100">
-                    ABOUT <span className='text-seance-700'>ME</span></h1>
+                <h2 className="text-center sm:text-left text-3xl sm:text-5xl font-semibold mb-1 lg:mb-4 dark:text-slate-100">
+                    ABOUT <span className='text-seance-700'>ME</span></h2>
                 <p className='sm:text-lg mt-4 md:mt-8 text-justify dark:text-slate-200'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni error perspiciatis quae quod? Quaerat veritatis voluptatem, sed eum eligendi quas, quae maiores suscipit deleniti, iste sapiente beatae voluptas perferendis non?</p>
             </div>
             <div className="flex justify-center w-full mt-10 md:mt-0 pt-0 md:pt-10">
