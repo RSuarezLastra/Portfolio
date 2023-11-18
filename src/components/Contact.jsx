7163622575a8fd29e2b2b0cda514b7d927739a91
RSLastra <raul.slastra@gmail.com>
2023-11-17 22:33:36 -0600

    envio de main por formulario

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,40 @@
+import { useState } from 'react';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import ButtonTwo from './buttonTwo';
 import { FiPhone, FiMail, FiMapPin } from 'react-icons/fi';
-import { IoMdSend  } from 'react-icons/io';
+import { IoMdSend } from 'react-icons/io';
+import axios from 'axios';
 
 const Contact = () => {
+
+    const [form, setForm] = useState({
+        name: '',
+        email: '',
+        message: ''
+    })
+
+    const handleChange = (e) => {
+        const { id, value } = e.target;
+        setForm({ ...form, [id]: value })
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await axios.post('https://formsubmit.co/raul.slastra@gmail.com', form);
+
+            if (response.status === 200) {
+                console.log('Correo electrónico enviado con éxito');
+                document.getElementById('contact-form').reset();
+            } else {
+                console.error('Error al enviar el correo electrónico');
+            }
+        } catch (error) {
+            console.error('Error de red:', error);
+        }
+    }
     return (
         <section id='contact' className='h-screen w-3/4 lg:w-2/3 mx-auto scroll-mt-24'>
             <h2 className="text-center font-semibold text-3xl mb-8 dark:text-slate-100">CONTACT <span className='text-seance-700'>ME</span></h2>
@@ -23,46 +53,54 @@ const Contact = () => {
                             <p className='ml-4 text-lg dark:text-white'>+52 9933184599</p>
                         </div>
                         <div className='flex'>
-                            <FiMapPin  size={30} className='text-seance-800' />
+                            <FiMapPin size={30} className='text-seance-800' />
                             <p className='ml-4 text-lg dark:text-white'>Villahermosa, México</p>
                         </div>
                     </div>
                 </div>
                 <div className='py-10 '>
                     <Box
+                        id="contact-form"
+                        onSubmit={handleSubmit}
                         className='bg-slate-100 rounded-xl p-5'
                         component="form">
                         <h3 className="text-center font-semibold text-3xl mb-6" >Send a message</h3>
                         <div >
                             <TextField
-                                id='standard-basic'
+                                id='name'
+                                name='name'
                                 label='Your Name'
                                 type='text'
                                 variant='standard'
                                 fullWidth
-                                required />
+                                required
+                                onChange={handleChange} />
                         </div>
                         <div className='mt-10'>
                             <TextField
-                                id='standard-basic'
+                                id='email'
+                                name='email'
                                 label='Your Email'
                                 type='email'
                                 variant='standard'
                                 fullWidth
-                                required />
+                                required
+                                onChange={handleChange} />
                         </div>
                         <div className='mt-10'>
                             <TextField
-                                id='standard-basic'
+                                id='message'
+                                name='message'
                                 label='Your Message'
-                                type='email'
+                                type='text'
                                 variant='standard'
                                 fullWidth
                                 multiline
-                                required />
+                                required
+                                onChange={handleChange} />
                         </div>
                         <div className='mt-20 grid place-items-end'>
-                            <ButtonTwo label='SEND' icon={<IoMdSend/>} />
+                            <ButtonTwo label='SEND' icon={<IoMdSend />} />
                         </div>
                     </Box>
 
