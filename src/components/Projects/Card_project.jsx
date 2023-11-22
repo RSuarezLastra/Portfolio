ddb8ebd9d27c08d5a8cd2005bef43cfbda482e78
RSLastra <raul.slastra@gmail.com>
2023-11-22 13:33:45 -0600

    mejorando responsive project card

--- src/components/Projects/Card_project.jsx
+++ src/components/Projects/Card_project.jsx
@@ -17,20 +17,20 @@ const Card_project = ({ title, description, tags, imgUrl }) => {
                 scale: scaleProgres,
                 opacity: opacityProgres
             }}
-            className="bg-gray-50  dark:bg-neutral-800  max-w-[42rem] border border-black/10 overflow-hidden rounded-lg sm:pr-8 relative h-[20rem] mb-4 sm:mb-8 group group-even:pl-8 ">
-            <div className="pt-4 pb-3 sm:pb-6 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[19rem] ">
-                <h3 className="text-2xl font-semibold dark:text-slate-100">{title}</h3>
+            className="bg-gray-50  dark:bg-neutral-800  max-w-[42rem] border border-black/10 overflow-hidden rounded-lg pr-0 lg:pr-6 relative h-[20rem] mb-4 sm:mb-8 group group-even:pl-8 ">
+            <div className="pt-4 pb-3 sm:pb-6 px-5 sm:pl-6 sm:pr-2 md:pt-6 lg:pt-10 sm:max-w-[50%] flex flex-col h-full md:group-even:ml-[18.25rem] lg:group-even:ml-[20rem]">
+                <h3 className="text-lg md:text-xl lg:text-2xl font-semibold dark:text-slate-100">{title}</h3>
                 <p className="mt-2 leading-relaxed dark:text-slate-100">{description}</p>
                 <ul className="flex flex-wrap gap-2 mt-4 sm:mt-auto">
                     {tags.map((tag, index) => (
-                        <li key={index} className=" text-white rounded-full uppercase px-3 py-1 tracking-wider  bg-neutral-600">{tag}</li>
+                        <li key={index} className=" text-white text-xs md:text-base  rounded-full uppercase px-2 lg:px-3 py-1 tracking-wider  bg-seance-800">{tag}</li>
                     ))}
                 </ul>
             </div>
             <img
                 src={imgUrl}
                 alt={title}
-                className="absolute bottom-0 -right-40 w-[28.25rem] rounded-t-xl shadow-2xl group-even:right-[initial] group-even:-left-40  h-64 " />
+                className="absolute bottom-0 -right-40 w-[28.25rem] rounded-t-xl shadow-2xl group-even:right-[initial] group-even:-left-40  sm:h-56 md:h-64 " />
         </motion.section>
 
     );
