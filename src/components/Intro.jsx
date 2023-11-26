f8fe5a2980e70ff3c4fe0eae18f55f45b65db7b5
RSLastra <raul.slastra@gmail.com>
2023-11-26 15:07:42 -0600

    seccion about

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -7,8 +7,8 @@ const Intro = () => {
         <section id='about' className=' min-h-[26rem] sm:min-h-[42rem] scroll-mt-24'>
             <h2 className="text-center  text-3xl sm:text-5xl font-semibold  dark:text-slate-100">
                 ABOUT <span className='text-seance-700'>ME</span></h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-0 md:gap-10   p-4 md:px-10 lg:px-12  sm:w-3/4 mx-auto mb-8">
-                <div className="flex flex-col justify-center">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-0 md:gap-8   py-4 w-full md:w-5/6 mx-auto mb-8">
+                <div className="flex flex-col justify-center md:px-4 lg:px-6">
                     <p className='sm:text-xl mt-4 md:mt-8 text-justify dark:text-slate-200'>Hello, I'm Raul Suarez Lastra, a full stack developer, with a background in mechanical engineering. During my degree, I learned about programming and ended up liking it more than my own career.
                         In addition to developing web applications, I like to play guitar and sing.</p>
                 </div>
